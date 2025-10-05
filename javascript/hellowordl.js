b26064b56bdce6fbb5a330d9f951996323def1b4
Minaal S Naik <minaalsnaik@gmail.com>
2025-10-05 10:01:50 +0530

    Updated Javascript hellowordl code

--- javascript/hellowordl.js
+++ javascript/hellowordl.js
@@ -1,4 +1,8 @@
-console.log("Hello, World!");
-let a = 10;
-let b = 20;
-console.log(a+b);
+let x = Number(prompt("Enter the value of x : "));
+let y = Number(prompt("Enter the value of y : "));
+
+let z = x + y;
+
+console.log("The sum of x and y is : " + z);
+
+console.log("Hello World!");
