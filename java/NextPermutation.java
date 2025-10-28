e4e26410b55cc6b70eda5764b07157210a5eb2fa
Janaka Ishan Senarathna <107669525+Janakaishansenarathna@users.noreply.github.com>
2025-10-28 10:38:45 +0530

    Add Next Permutation in Java
    This PR adds a new file java/NextPermutation.java which implements the next lexicographical permutation algorithm for an array of numbers. The program demonstrates how to generate the next permutation in-place.

---
+++ java/NextPermutation.java
@@ -0,0 +1,33 @@
+// NextPermutation.java
+// Find the next lexicographical permutation of an array of numbers
+public class NextPermutation {
+    public static void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) i--;
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (nums[j] <= nums[i]) j--;
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1, nums.length - 1);
+    }
+    private static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    private static void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            swap(nums, start, end);
+            start++;
+            end--;
+        }
+    }
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3};
+        nextPermutation(nums);
+        System.out.print("Next permutation: ");
+        for (int num : nums) System.out.print(num + " ");
+        System.out.println();
+    }
+}
